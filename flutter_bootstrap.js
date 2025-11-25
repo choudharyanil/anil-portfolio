764c7dd95072fc8341b8a41d2f361912c84f4b38
choudharyanil <32925128+choudharyanil@users.noreply.github.com>
2025-11-25 06:27:45 +0000

    Deploying to gh-pages from @ choudharyanil/anil-portfolio@333522312224352ad3197026ecd733fa8dfd1a87 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4268687799"
+    serviceWorkerVersion: "239897593"
   }
 });
